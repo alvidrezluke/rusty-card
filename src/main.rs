c2bb980fc8120720f6956f10f8321b193de0eaf7
Luke Alvidrez <alvidrezluke@gmail.com>
2022-05-24 17:05:12 +0000

    Initialized basic discord bot

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+use dotenv::dotenv;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content.chars().take(1).collect::<String>() == "!" && msg.author != msg.{
+            if msg.content == "!ping" {
+                if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+        } else {
+            println!("Not talking to me");
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
